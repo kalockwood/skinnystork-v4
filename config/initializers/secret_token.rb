55ca3de9b6f44b0252e095be03d38e9a1e9e7589
kalockwood <kathleen@Kathleens-MacBook-Pro.local>
2014-08-05 18:32:48 -0500

    first commit for new repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SkinnystorkV4::Application.config.secret_key_base = '3b47e51a0abdf69c75c39c4969477f561259d04d16a6e27f3727a1ab203764f18caebfca184a3b7020a8ac87ea638c88a3f6c0d4d77d760ef4747759d51f445e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SkinnystorkV4::Application.config.session_store :cookie_store, key: '_skinnystork-v4_session'
