8c3a7ec2165d026fab7234f8f603c650005a2625
Kalockwood <kathleen@skinnystork.com>
2014-08-13 12:26:40 -0500

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,16 +1,35 @@
 require 'spec_helper'
 
-describe "Static Pages" do
-  describe "Home page" do
+describe "Static pages" do
 
-    it "should have the content 'Home'" do
-    	visit '/static_pages/home'
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      	expect(page).to have_content('home')
-    end
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("SkinnyStork")
-    end
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_content('SkinnyStork') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
+  end
+
+  describe "Help page" do
+    before { visit help_path }
+
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_content('About') }
+    it { should have_title(full_title('About')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
