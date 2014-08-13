0ff2ea45919652c2c9aba4539a8e696715219667
Kalockwood <kathleen@skinnystork.com>
2014-08-12 21:14:29 -0500

    Ready to Fill out Layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static Pages" do
   describe "Home page" do
 
     it "should have the content 'Home'" do
@@ -8,9 +8,9 @@ describe "StaticPages" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       	expect(page).to have_content('home')
     end
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Home")
+      expect(page).to have_title("SkinnyStork")
     end
   end
 end
