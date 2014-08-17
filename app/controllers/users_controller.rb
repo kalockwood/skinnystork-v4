aae5812a744bcac16e222a8259dce3d3a56ba237
Kalockwood <kathleen@skinnystork.com>
2014-08-17 15:32:43 -0500

    Finished User Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  #before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
   # GET /users.json
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:id])
   end
 
   # GET /users/new
@@ -24,18 +25,26 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
+      @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @user }
+      if @user.save 
+        flash[:success] = "Welcome to SkinnyStork!"
+        redirect_to @user
+        #handle a successful save.
+        #format.html { redirect_to @user, notice: 'User was successfully created.' }
+        #format.json { render action: 'show', status: :created, location: @user }
       else
-        format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        #format.html { render action: 'new' }
+        #format.json { render json:  @user.errors, status: :unprocessable_entity }
+        render 'new'
       end
-    end
   end
+
+  private
+
+      def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
@@ -60,15 +69,4 @@ class UsersController < ApplicationController
       format.json { head :no_content }
     end
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:name, :email)
-    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,8 @@ require 'spec_helper'
 
 describe User do
 	before do
-	  @user = User.new(name: "Example User", email: "user@example.com",
-	  					password: "foobar", password_confirmation: "foobar")
-
+	  @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+  end
   subject { @user }
 
   it { should respond_to(:name) }
@@ -61,27 +60,27 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "when password is not present" do
+    before do
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+  before { @user.password_confirmation = "mismatch" }
+  it { should_not be_valid }
+  end
+
   describe "with a password that's too short" do
   	before { @user.password = @user.password_confirmation = "a" * 5 }
   	it { should be_invalid }
   end
 
-  describe "when password is not present" do
-  	before do
-    @user = User.new(name: "Example User", email: "user@example.com",
-                     password: "foobar", password_confirmation: "foobar")
-  	end
-  	it { should_not be_valid }
-  end
-	describe "when password doesn't match confirmation" do
-  before { @user.password_confirmation = "mismatch" }
-  it { should_not be_valid }
-	end
-
 describe "return value of authenticate method" do
   before { @user.save }
   let(:found_user) { User.find_by(email: @user.email) }
-end
 
   describe "with valid password" do
     it { should eq found_user.authenticate(@user.password) }
